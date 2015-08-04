ef4f706fc3d44c147de65f8f56467eb15e162f29
kristenlk <klkehlenbeck@gmail.com>
2015-08-03 21:23:46 -0400

    Fixes sign-in bug (misnamed JSON). Renames some elements in index.html. Beginning to implement AJAX for register.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var token;
+var userID;
+
 $(document).ready(function(){
 
 // Click handlers for register / sign in modals
@@ -23,10 +26,125 @@ $(document).ready(function(){
     };
   });
 
+// Click handlers for sign in functionality
+
+  var sa = 'http://localhost:3000';
+
+  $('#register-btn').on('click', function(){
+    var profile = {moniker:
+                    if ($('#anon-btn').is(':checked')) {
+                      return 'anonymous';
+                    else {
+                      return $('#register-moniker').val();
+                    },
+                    location: $('#register-location').val(),
+                    email_or_phone: $('input[name=phone-or-email]:checked').val(),
+                    selected_time: $('input[name=time-of-day]:checked').val()
+                  };
+
+    $.ajax(sa + '/create', {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        user: {
+          email: $('#register-email').val(),
+          password: $('#register-password').val(),
+          password_confirmation: $('#register-confirm-password').val()
+        },
+        profile: profile;
+      }),
+      dataType: 'json',
+      method: 'POST'
+    }).done(function(data, textStatus, jqxhr){
+      $('#result').val(JSON.stringify(data));
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      $('#result').val('registration failed');
+    });
+  });
+
+  $('#sign-in-btn').on('click', function(){
+    $.ajax(sa + '/login', {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        credentials: {
+          email: $('#sign-in-email').val(),
+          password: $('#sign-in-password').val(),
+        }
+      }),
+      dataType: 'json',
+      method: 'POST'
+    }).done(function(data, textStatus, jqxhr){
+      console.log(data);
+      $('.modal').removeClass('show');
+      token = data.user_login.token;
+      userID = data.user_login.id;
+      // simplestorage
+      console.log(userID);
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      $('#login-alert').addClass('show');
+      // console.log("We were unable to locate an account with that email address and password combination. Please try again.");
+    });
+  });
+
+  // $('#list').on('click', function(){
+  //   $.ajax(sa + '/users', {
+  //     dataType: 'json',
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: 'Token token=' + $('#token').val()
+  //     }
+  //   }).done(function(data, textStatus, jqxhr){
+  //     $('#result').val(JSON.stringify(data));
+  //   }).fail(function(jqxhr, textStatus, errorThrown){
+  //     $('#result').val('list failed');
+  //   });
+  // });
+
+  // $('#create').on('click', function(){
+  //   $.ajax(sa + '/games', {
+  //     contentType: 'application/json',
+  //     processData: false,
+  //     data: JSON.stringify({}),
+  //     dataType: 'json',
+  //     method: 'POST',
+  //     headers: {
+  //       Authorization: 'Token token=' + $('#token').val()
+  //     }
+  //   }).done(function(data, textStatus, jqxhr){
+  //     $('#result').val(JSON.stringify(data));
+  //   }).fail(function(jqxhr, textStatus, errorThrown){
+  //     $('#result').val('create failed');
+  //   });
+  // });
+
 // Click handlers for send a message modal
   $('#header-msg').on('click', function(){
     $('#send-message-modal').addClass('show');
   });
+
+// Click handlers for creating a message
+
+  $("#send-msg-btn").on('click', function() {
+    debugger;
+    $.ajax({
+      url: sa + '/messages',
+      method: 'POST',
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      data: {
+        message: {
+          body: $("#message-text").val()
+        }
+      }
+    }).done(function(data) {
+      console.log("Created message!");
+    }).fail(function(data) {
+      console.error(data);
+    });
+  });
+
 
 });
 
