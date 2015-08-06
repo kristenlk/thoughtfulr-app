4a8bff0577df839dc325400f57f2668f9518a955
kristenlk <klkehlenbeck@gmail.com>
2015-08-06 00:02:31 -0400

    Adds show actions for messages and users / profiles. Adds and utilizes handlebars to display user account data.

--- main.js
+++ main.js
@@ -2,22 +2,31 @@ var token;
 var userID;
 var moniker;
 var tokenExists;
+var logOut;
 
 $(document).ready(function(){
 
   // Checks for a token, does stuff if it finds one
+
   tokenExists = function(token){
     if (token) {
       $('.register, .sign-in').addClass('hide');
       $('.my-account, .log-out, .temp-send-text').removeClass('hide');
     } else {
-      console.log('no token yet')
+      console.log('no token found!')
+      $('.my-account, .log-out, .temp-send-text').addClass('hide');
+      $('.register, .sign-in').removeClass('hide');
     }
   };
 
   tokenExists(token);
 
-// Click handlers for register / sign in modals
+  logOut = function(){
+    token = null;
+  };
+
+  // Click handlers for header
+
   $('.sign-in').on('click', function() {
     $('#register-modal').removeClass('show');
     $('#sign-in-modal').addClass('show');
@@ -28,9 +37,19 @@ $(document).ready(function(){
     $('#register-modal').addClass('show');
   });
 
+  $('.log-out').on('click', function(){
+    logOut();
+    tokenExists(token);
+    $('#user-account').removeClass('show').addClass('hide');
+  });
+
+  // Click handler to close any open modal
+
   $('.x').on('click', function() {
     $('.modal').removeClass('show');
   });
+
+  // Click handlers for register modal
 
   $('[name="phone-or-email"]').on('click', function() {
     if ($('#phone-radio-btn').is(':checked')) {
@@ -47,6 +66,11 @@ $(document).ready(function(){
   $('#register-moniker').on('click', function() {
     $('#anon-btn').prop('checked', false);
   });
+
+
+
+
+
 
 // Click handlers for sign in functionality
 
@@ -157,7 +181,8 @@ $(document).ready(function(){
   // });
 
 // Click handlers for send a message modal
-  $('#header-msg').on('click', function(){
+
+  $('.send-msg').on('click', function(){
     $('#send-message-modal').addClass('show');
   });
 
@@ -187,17 +212,24 @@ $(document).ready(function(){
   // Click handler for displaying My Account information
 
   $('.my-account').on('click', function(){
-    $('#my-account-nav').removeClass('hide').addClass('show');
+    $('#user-account, #my-account-nav').removeClass('hide').addClass('show');
     $.ajax({
       url: sa + '/received_messages',
       headers: {
         Authorization: 'Token token=' + token
       }
     }).done(function(data) {
-      // $('#account-stuff').html(data)
+      var html;
+      $('#account-info > div').addClass('hide');
       var templatingFunction = Handlebars.compile($('#received-messages-template').html());
-      var html = templatingFunction({receivedmessage: data.messages});
-      $('#display-received-messages').html(html);
+      console.log(data)
+      if (data.messages.length > 0) {
+        html = templatingFunction({receivedmessage: data.messages});
+      } else {
+        html = 'You haven\'t received any messages yet. If you haven\'t sent out a message yet, <a href="#" class="send-msg">get sending</a> and you\'ll receive a message within the next day. If you\'ve already sent a message, you should be receiving your first message within the next 24 hours.'
+      }
+      $('#display-received-messages').removeClass('hide').html(html);
+
     }).fail(function(data) {
       console.error(data);
     });
@@ -211,11 +243,17 @@ $(document).ready(function(){
         Authorization: 'Token token=' + token
       }
     }).done(function(data) {
-      // $('#account-stuff').html(data)
+      var html;
       $('#account-info > div').addClass('hide');
       var templatingFunction = Handlebars.compile($('#received-messages-template').html());
-      var html = templatingFunction({receivedmessage: data.messages});
+      console.log(data)
+      if (data.messages.length > 0) {
+        html = templatingFunction({receivedmessage: data.messages});
+      } else {
+        html = 'You haven\'t received any messages yet. If you haven\'t sent out a message yet, <a href="#" class="send-msg">get sending</a> and you\'ll receive a message within the next day. If you\'ve already sent a message, you should be receiving your first message within the next 24 hours.'
+      }
       $('#display-received-messages').removeClass('hide').html(html);
+
     }).fail(function(data) {
       console.error(data);
     });
@@ -230,11 +268,17 @@ $(document).ready(function(){
       }
     }).done(function(data) {
       // $('#account-stuff').html(data)
+      console.log(data)
+      var html;
       $('#account-info > div').addClass('hide');
-
       var templatingFunction = Handlebars.compile($('#sent-messages-template').html());
-      var html = templatingFunction({sentmessage: data.messages});
+      if (data.messages.length > 0) {
+        html = templatingFunction({sentmessage: data.messages});
+      } else {
+        html = 'You have not yet sent any messages. You won\'t receive a daily messages until you send one, so <a href="#" class="send-msg">get sending!</a>'
+      }
       $('#display-sent-messages').removeClass('hide').html(html);
+
     }).fail(function(data) {
       console.error(data);
     });
@@ -250,13 +294,53 @@ $(document).ready(function(){
     }).done(function(data) {
       $('#account-info > div').addClass('hide');
       console.log(data);
-      var templatingFunction = Handlebars.compile($('#account-settings-template').html());
+      var templatingFunction = Handlebars.compile($('#account-settings-template-profile').html());
       var html = templatingFunction(data);
+      // only show a phone number field and the current phone number if a phone number exists in the json that's sent back
+
       $('#display-account-settings').removeClass('hide').html(html);
     }).fail(function(data) {
       console.error(data);
     });
+
+    $.ajax({
+      url: sa + '/users/' + userID,
+      headers: {
+        Authorization: 'Token token=' + token
+      }
+    }).done(function(data) {
+      console.log(data);
+      var templatingFunction = Handlebars.compile($('#account-settings-template-user').html());
+      var html = templatingFunction(data);
+      $('#display-account-settings').prepend(html);
+    }).fail(function(data) {
+      console.error(data);
+    });
   });
+
+
+// Account info / preferences:
+  // Click handler for editing email address
+
+  // Click handler for editing "phone or email"
+
+  // Click handler for editing phone number
+
+  // Click handler for editing moniker (if user is anonymous, anonymous radio button is selected. If user isn't anonymous, current moniker is changed to anonymous if they select that radio button.)
+
+  // Click handler for editing location
+
+  // Click handler for opting into / out of alerts
+
+
+// Sent messages:
+  // Click handler for editing a sent message
+
+  // Click handler for deleting a sent message
+
+
+//
+
 
 
   // Temporary click handler for Send Message button (to generate a text via Twilio API)
